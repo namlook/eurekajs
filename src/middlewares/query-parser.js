716da78bf8d75db77ebbed2cfe3e3beccec46a38
namlook <nicolas.clairon@elkorado.com>
2015-06-06 14:21:21 +0200

    send bad request if query parser returns errors

--- src/middlewares/query-parser.js
+++ src/middlewares/query-parser.js
@@ -3,5 +3,11 @@ import QueryParser from '../utils/query-parser';
 
 export default function(req, res, next) {
     req.parsedQuery = new QueryParser(req.db, req.resource.name, req.query);
+
+    var errors = req.parsedQuery.errors;
+    if (errors) {
+        return res.badRequest('bad request', {'issue': 'bad query', reasons: errors});
+    }
+
     return next();
 }
